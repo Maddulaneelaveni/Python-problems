663844ed898adf3b950c3b9c8df9b32dd2d27a4b
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-01-22 22:00:00 +0530

    file updated

--- STAR_PATTERNS/Square_Star_Pattern.py
+++ STAR_PATTERNS/Square_Star_Pattern.py
@@ -1,13 +1,23 @@
 # Square Star Pattern
 
+# *****
+# *****
+# *****
+# *****
+# *****
+
+
 n = int(input("Enter a number: ")) # size of the square
 for i in range(n):
     for j in range(n):
         print("*", end="")
     print()
 
+# Alternative simpler version without nested loops using string multiplication 
+# string multiplication means repeating a string multiple times by multiplying it with an integer.
 
 n = 5
 for i in range(n):
     print("*" * n)
+
 
