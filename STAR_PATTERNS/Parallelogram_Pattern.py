482e636515d4f0dbf79ac1dd7191c93a8e066e39
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-01-27 12:36:27 +0530

    parallelogram pattern added

--- STAR_PATTERNS/Parallelogram_Pattern.py
+++ STAR_PATTERNS/Parallelogram_Pattern.py
@@ -3,3 +3,20 @@
 #    *****
 #   *****
 #  *****
+
+
+n = 5  # number of rows
+for i in range(n):
+    # Print leading spaces
+    for j in range(n - i - 1):
+        print(" ", end="")
+    # Print stars
+    for k in range(n):
+        print("*", end="")
+    print()
+
+# Alternative simpler version without nested loops using string multiplication
+n = 5  # number of rows
+for i in range(n):
+    print(" " * (n - i - 1) + "*" * n)
+    
