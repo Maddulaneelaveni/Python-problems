c86d09501cda8900d5934fe6fbacf32d156ea1c4
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-01-31 12:30:36 +0530

    Hallow pyramid pattern added

--- STAR_PATTERNS/Hollow_pyramid.py
+++ STAR_PATTERNS/Hollow_pyramid.py
@@ -1,5 +1,9 @@
 # Hallow pyramid pattern
-
+#     *
+#   * *
+#  *   *
+# *     *
+#*********
 n = int(input("Enter number of rows: "))
 for i in range(1, n + 1): # for each row
     for j in range(n - i): # print leading spaces
