0adb8d763f6d25ae5a434e77e0ca54af8e053a8a
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-02-01 20:58:07 +0530

    Inverted pyramid pattern added

--- STAR_PATTERNS/Inverted_pyramid.py
+++ STAR_PATTERNS/Inverted_pyramid.py
@@ -4,3 +4,10 @@
 #   *****
 #    ***
 #     *
+n = int(input("Enter number of rows: "))
+for i in range(n, 0, -1):
+    for space in range(n - i):
+        print(" ", end="")
+    for star in range(1, 2 * i):
+        print("*", end="")
+    print()
